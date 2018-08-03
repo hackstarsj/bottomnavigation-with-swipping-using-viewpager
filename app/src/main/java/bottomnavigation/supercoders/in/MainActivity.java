da189dbb7b9e64847f258a088771242ab453b8d5
sanjeev <sksanjeev362@gmail.com>
2018-08-03 15:02:20 +0530

    Android Bottom Naviagtion With Swipping Using ViewPager

---
+++ app/src/main/java/bottomnavigation/supercoders/in/MainActivity.java
@@ -0,0 +1,94 @@
+package bottomnavigation.supercoders.in;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import bottomnavigation.supercoders.in.adapter.ViewPagerAdapter;
+import bottomnavigation.supercoders.in.fragment.ChatFragment;
+import bottomnavigation.supercoders.in.fragment.HomeFragment;
+import bottomnavigation.supercoders.in.fragment.StatusFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    MenuItem prevMenuItem;
+
+    //============Handle Bottom Navgigation Click and Change Page on ViewPager Adapter============
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    viewPager.setCurrentItem(0);
+                    return true;
+                case R.id.navigation_dashboard:
+                    viewPager.setCurrentItem(1);
+                    return true;
+                case R.id.navigation_notifications:
+                    viewPager.setCurrentItem(3);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        final BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (prevMenuItem != null) {
+                    prevMenuItem.setChecked(false);
+                }
+                else
+                {
+                    navigation.getMenu().getItem(0).setChecked(false);
+                }
+                navigation.getMenu().getItem(position).setChecked(true);
+                prevMenuItem = navigation.getMenu().getItem(position);
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        setPager(viewPager);
+
+    }
+
+
+    private void setPager(ViewPager viewPager)
+    {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new HomeFragment());
+        adapter.addFragment(new ChatFragment());
+        adapter.addFragment(new StatusFragment());
+        viewPager.setAdapter(adapter);
+    }
+
+}
---
+++ app/src/main/java/bottomnavigation/supercoders/in/fragment/StatusFragment.java
@@ -0,0 +1,36 @@
+package bottomnavigation.supercoders.in.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import bottomnavigation.supercoders.in.R;
+
+
+public class StatusFragment extends Fragment {
+
+    public StatusFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_status, container, false);
+    }
+
+
+}
